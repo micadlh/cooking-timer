f2b38b598c033ad884cf91033e8e751e620db0f1
Mica <micaeladelahera@gmail.com>
2025-02-14 09:52:32 +0100

    quite html  y  menu por defecto

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600
+        width: 400,
+        height: 500,
+        minHeight: 100,
+        minWidth: 100
     })
 
     win.loadFile('index.html')
 }
+
+Menu.setApplicationMenu(null);
 
 app.whenReady().then(() => {
     createWindow()
